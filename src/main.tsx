5378be152df1d56c19d111d6f9312f118efd00e0
hyunkyu-cpu <songhak4025@gmail.com>
2025-07-11 22:02:24 +0900

    Add basic login and student dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import TeacherApp from './teacher/TeacherApp'
 import './index.css' // 이 줄이 반드시 있어야 합니다!
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TeacherApp />
   </React.StrictMode>,
 )
